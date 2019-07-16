995e0849d1807b165278cbc8c4e1ae83dcd5d55b
José Eduardo Storopoli <thestoropoli@gmail.com>
2019-07-16 18:55:52 -0400

    Update communalities_optimal_solution.R

--- communalities_optimal_solution.R
+++ communalities_optimal_solution.R
@@ -1,12 +1,12 @@
 communalities_optimal_solution <- function(df, nfactors, rotate="varimax"){
-  library(psych)
+  require(psych)
   source("how_many_factors.R")
   source("printLoadings.R")
   removed <- c()
   results <- principal(df, nfactors = nfactors, rotate = rotate, scores = T)
   #results$communality <- as.data.frame(results$communality)
   while (any(as.data.frame(as.data.frame(results$communality)) < 0.5)){
-    message(sprintf("There is still an individual communality value below 0.5: "), 
+    message(sprintf("There is still an individual communality value below 0.5: "),
             rownames(as.data.frame(results$communality))[which.min(apply(as.data.frame(results$communality),MARGIN=1,min))]," - ",
             min(as.data.frame(results$communality)))
     column <- sprintf(rownames(as.data.frame(results$communality))[which.min(apply(as.data.frame(results$communality),MARGIN=1,min))])
@@ -15,19 +15,19 @@ communalities_optimal_solution <- function(df, nfactors, rotate="varimax"){
     #row <- as.numeric(gsub("([0-9]+).*$", "\\1", row))
     df <- df[, !(colnames(df) %in% column), drop=FALSE]
     #df <- df[-row, ]
-    #rownames(df) <- NULL 
+    #rownames(df) <- NULL
     results <- principal(df, nfactors = nfactors, scores = T)
   }
-  
+
   #write.csv(results$communality, "communalities_individual.csv")
   how_many_factors(df)
-  
+
   loadings <- as.table(printLoadings(results$loadings))
-  
+
   fa.diagram(results, digits = 3, cut = 0.4, sort = T) # Diagram of Factors, items and loadings
-  
+
   plot(results)
-  
+
   return(list(
     df = df,
     removed = removed,
