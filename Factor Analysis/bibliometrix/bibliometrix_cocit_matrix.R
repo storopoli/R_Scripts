d3e8cab6a7af9957625e1d01dab0f7aca88ff44f
José Eduardo Storopoli <thestoropoli@gmail.com>
2020-05-03 20:43:58 -0300

    Updated Bibliometrix to work with top-N-cited cocitation

--- Factor Analysis/bibliometrix/bibliometrix_cocit_matrix.R
+++ Factor Analysis/bibliometrix/bibliometrix_cocit_matrix.R
@@ -1,42 +1,177 @@
 library(bibliometrix)
+library(dplyr)
+library(nFactors)
+library(psych)
+library(openxlsx)
+library(Matrix)
+library(igraph)
+library(network)
+library(intergraph)
+set.seed(123)
 
-# se for usar direto da Scopus
-D <- readFiles("~/Google Drive/Academic/Orientações/Jefferson da Costa/RSL/BibTeX_Scopus/158_stakeholder.bib")
-M <- convert2df(D, format = "bibtex", dbsource = "scopus")
-write.csv2(M, "Bruto_Bibliometrix.csv")
+M <- readRDS("data/all_records.rds")
 
-# import the clean dataset to R
-M <- read.csv2("Bruto_Bibliometrix_limpo.csv",
-               colClasses = rep('character', 62))
+# Citation Analysis  ---------------------------------------------
+# top-cited 
+CR <- as_tibble(citations(M, field = "article", sep = ";")$Cited)
 
-# Citation Matrix
-M$CR<- stringr::str_replace_all(as.character(M$CR),"DOI;","DOI ")
+total_cit <- sum(CR$n) # total citations (8,775)
+lotka <- 0.05 * total_cit # Lotka threshold (439)
 
-# Cocitation
-cocit <- biblioNetwork(M, analysis = "co-citation",
-                          network = "references", sep = ";", shortlabel = T)
-cocit <- cocit[nchar(colnames(cocit)) != 0,
-                     nchar(colnames(cocit)) != 0]
+CR %>%
+  mutate(cum_sum = cumsum(n)) %>% 
+  filter(cum_sum <= 439) %>% 
+  tail()
+
+top_cited <- CR %>% # 57 top-cited (min 4 citations)
+  mutate(cum_sum = cumsum(n)) %>% 
+  filter(n >= 4)
+
+
+write.xlsx(CR, file = "tables/cited_references.xlsx")
+
+
+# Co-citation -------------------------------------------------------------
+
+# Normalize top-cited to be equal as cocMatrix
+reduceRefs<- function(A){
+  
+  ind=unlist(regexec("*V[0-9]", A))
+  A[ind>-1]=substr(A[ind>-1],1,(ind[ind>-1]-1))
+  ind=unlist(regexec("*DOI ", A))
+  A[ind>-1]=substr(A[ind>-1],1,(ind[ind>-1]-1))
+  return(A)
+}
+
+Fi <-gsub("DOI;","DOI ",as.character(top_cited$CR))
+sep <- ";"
+Fi <- strsplit(Fi,sep)
+Fi <- lapply(Fi,trim.leading)
+Fi <- lapply(Fi,function(l) l<-l[nchar(l)>10])
+
+uniqueField <- unique(unlist(Fi))
+uniqueField <- uniqueField[!is.na(uniqueField)]
+
+
+S <- gsub("\\).*",")",uniqueField)
+S <- gsub(","," ",S)
+S <- gsub(";"," ",S)
+S <- reduceRefs(S)
+uniqueField <- unique(trimES(S))
+Fi <- lapply(Fi, function(l){
+  l <- gsub("\\).*",")",l)
+  l <- gsub(","," ",l)
+  l <- gsub(";"," ",l)
+  l <- l[nchar(l)>0]
+  l <- reduceRefs(l)
+  l <- trimES(l)
+  return(l)
+})
+
+# Co-citation Matrix
+cocit <- cocMatrix(M, Field = "CR", sep = ";", binary = F)
+cocit <- crossprod(cocit, cocit)
 dim(cocit)
 
-# top-cited 
-CR <- citations(M, field = "article", sep = ";")
-topcited <-rownames(cbind(CR$Cited[1:1300])) # escolher top-cited para cortar na co-cit
-cited <- Matrix::diag(cocit)
-cited <- as.data.frame(cbind(rownames(cocit), cited))[order(-cited),]
-# Export top-cited to CSV
-write.csv2(cited, "top-cited.csv")
+cocit <- cocit[rownames(cocit) %in% Fi,
+               colnames(cocit) %in% Fi]
 
 # fix the diagonal to zero
-Matrix::diag(cocit) <- 0
+diag(cocit) <- 0
 
-# Subset Matrix
-# where cocit > threshold is the number of references in common
-threshold <- 14
-cocit_subset <- cocit[Matrix::rowSums(cocit >= threshold) >= 1,
-                            Matrix::colSums(cocit >= threshold) >= 1]
-cocit_matrix <- as.data.frame(as.matrix(cocit_subset))
-assertthat::are_equal(names(cocit_matrix), rownames(cocit_matrix))
+
+assertthat::are_equal(colnames(cocit), rownames(cocit))
 
 # Export CSV
-write.csv2(cocit_matrix, "cocit_matrix.csv")
+write.csv(as.matrix(cocit), "tables/cocit_matrix.csv")
+
+
+# PCA ---------------------------------------------------------------------
+
+# How Many Components?
+cocit <- as.matrix(cocit)
+ev <- eigen(cor(cocit)) # get eigenvalues
+ap <- parallel(subject = nrow(cocit),
+               var = ncol(cocit),
+               rep = 10000,
+               quantile = 0.05)
+nS <- nScree(x = ev$values, aparallel = ap$eigen$qevpea)
+png("images/co_cit_scree-plot.png", width = 14, height = 7, units = "in", res = 300,
+    type = "cairo-png", bg = "transparent")
+plotnScree(nS)
+dev.off()
+
+# PCA
+pca_cocit <- principal(cor(cocit), nfactors = 3, scores = T,
+                       rotate = "varimax")
+print(pca_cocit, digits = 2, cut = 0.4)
+# Export PCA to CSV
+write.csv(pca_cocit$loadings, "tables/pca_co_cit.csv")
+write.csv(pca_cocit$Vaccounted, "tables/variance_accounted_co_cit.csv")
+
+
+# Network Analysis --------------------------------------------------------
+
+# Export Factor ID for every variable
+# It is important to use absolute values because of negative loadings
+df <- data.frame(unclass(pca_cocit$loadings), stringsAsFactors = F)
+factors <- data.frame(
+  "ID" = rownames(df),
+  "factor" = apply(df[1:ncol(df)], 1, function(x) colnames(df)[which.max(abs(x))]),
+  stringsAsFactors = F)
+
+
+
+# Creating network using igraph
+cocit <- read.csv("tables/cocit_matrix.csv", row.names = 1) # if you need to load network
+g <- graph_from_adjacency_matrix(as.matrix(cocit), mode = "undirected",
+                                 weighted = T, diag = F)
+summary(g) #check graph
+V(g)$name # check vertex
+
+# Imputting the max absolute value loading as the factor for each Node
+factors$factor <- apply(factors[1:ncol(factors)], 1, function(x) colnames(factors)[which.max(abs(x))])
+
+# Vertex Attributes
+V(g)$name <- substr(rownames(factors), start=3, stop = 6)
+V(g)$ID <- rownames(factors)
+g <- set_vertex_attr(g, "factor", index = rownames(factors), value = factors$factor)
+V(g)$degree <- degree(g, normalized = F)
+V(g)$Ndegree <- degree(g, normalized = T)
+V(g)$betweeness <- betweenness(g, directed = F)
+V(g)$Nbetweeness <- betweenness(g, directed = F, normalized = T)
+V(g)$transitivity <- transitivity(g) # clustering coefficient
+V(g)$closeness <- closeness(g)
+names(get.vertex.attribute(g))
+
+write.csv(as.data.frame(vertex_attr(g)), "tables/cocit_vertex_attributes.csv")
+
+# Subnetworks
+sub_df <- data.frame()
+ties_between_group <- mixingmatrix(asNetwork(g), "factor")
+for (i in unique(na.omit(V(g)$factor))){
+  new_g <- induced_subgraph(g, which(V(g)$factor==as.character(i)))
+  total_size <- length(V(g))
+  size <- length(V(new_g))
+  centralization_degree <- centr_degree(new_g)$centralization
+  sub_density <- edge_density(new_g, loops = F)
+  sub_cohesion_igraph <- cohesion(new_g)
+  total_ties_between_factors <-  sum(ties_between_group$matrix[which(rownames(ties_between_group$matrix) %in% as.character(i)),
+                                                               -which(colnames(ties_between_group$matrix) %in% as.character(i))])
+  possible_ties_between_factors <- size * (total_size - size)
+  cohesion_manual <- sub_density * total_ties_between_factors / possible_ties_between_factors
+  dat <- data.frame("factor" = as.factor(i),
+                    "size"= size,
+                    "centralization_degree" = centralization_degree,
+                    "density" = sub_density,
+                    "cohesion" = sub_cohesion_igraph,
+                    "total_size_network" = total_size,
+                    "total_ties_between_factors" = total_ties_between_factors,
+                    "possible_ties_between_factors" = possible_ties_between_factors,
+                    "cohesion_manual" = cohesion_manual)
+  sub_df <- rbind(sub_df, dat)
+}
+write.csv2(sub_df, "tables/cocit_subnetwork_stats.csv")
+
+# Exporting to DOT (This is the best to Gephi)
+write_graph(g, "data/cocit_network.dot", format = "dot")
--- Factor Analysis/bibliometrix/bibliometrix_coupling_matrix.R
+++ Factor Analysis/bibliometrix/bibliometrix_coupling_matrix.R
@@ -1,33 +1,122 @@
 library(bibliometrix)
+library(dplyr)
+library(nFactors)
+library(psych)
+library(openxlsx)
+library(Matrix)
+library(igraph)
+library(network)
+library(intergraph)
+set.seed(123)
 
-# se for usar direto da Scopus
-D <- readFiles("~/Google Drive/Academic/Orientações/Jefferson da Costa/RSL/BibTeX_Scopus/158_stakeholder.bib")
-M <- convert2df(D, format = "bibtex", dbsource = "scopus")
-write.csv2(M, "Bruto_Bibliometrix.csv")
+M <- readRDS("data/all_records.rds")
 
-# import the clean dataset to R
-M <- read.csv2("Bruto_Bibliometrix_limpo.csv",
-               colClasses = rep('character', 62))
 
-# Citation Matrix
-M$CR<- stringr::str_replace_all(as.character(M$CR),"DOI;","DOI ")
+# Coupling ----------------------------------------------------------------
 
-# Bibliographic Coupling
-coupling <- biblioNetwork(M, analysis = "coupling",
-                          network = "references", sep = ";")
-coupling <- coupling[nchar(colnames(coupling)) != 0,
-                     nchar(colnames(coupling)) != 0]
-dim(coupling)
+# Coupling Matrix
+coup <- biblioNetwork(M, analysis = "coupling",
+                      network = "references", sep = ";")
+dim(coup)
+
 # fix the diagonal to zero
-Matrix::diag(coupling) <- 0
+diag(coup) <- 0
+
+assertthat::are_equal(colnames(coup), rownames(coup))
 
 # Subset Matrix
 # where coupling > threshold is the number of references in common
-threshold <- 3
-coupling_subset <- coupling[Matrix::rowSums(coupling >= threshold) >= 1,
-                            Matrix::colSums(coupling >= threshold) >= 1]
+threshold <- 6
+coupling_subset <- coup[Matrix::rowSums(coup >= threshold) >= 1,
+                        Matrix::colSums(coup >= threshold) >= 1]
 coupling_matrix <- as.data.frame(as.matrix(coupling_subset))
 assertthat::are_equal(names(coupling_matrix), rownames(coupling_matrix))
 
 # Export CSV
-write.csv2(coupling_matrix, "coupling_matrix.csv")
+write.csv2(as.matrix(coupling_matrix), "tables/coup_matrix.csv")
+
+
+# PCA ---------------------------------------------------------------------
+
+# How Many Components?
+ev <- eigen(cor(coupling_matrix)) # get eigenvalues
+ap <- parallel(subject = nrow(coupling_matrix),
+               var = ncol(coupling_matrix),
+               rep = 10000,
+               quantile = 0.05)
+nS <- nScree(x = ev$values, aparallel = ap$eigen$qevpea)
+png("images/coup_scree-plot.png", width = 14, height = 7, units = "in", res = 300,
+    type = "cairo-png", bg = "transparent")
+plotnScree(nS)
+dev.off()
+
+# PCA
+pca_coup <- principal(cor(coupling_matrix), nfactors = 4, scores = T,
+                      rotate = "varimax")
+print(pca_coup, digits = 2, cut = 0.4)
+# Export PCA to CSV
+write.csv2(pca_coup$loadings, "tables/pca_coup.csv")
+write.csv2(pca_coup$Vaccounted, "tables/variance_accounted_coup.csv")
+
+
+# Network Analysis --------------------------------------------------------
+
+# Export Factor ID for every variable
+# It is important to use absolute values because of negative loadings
+df <- data.frame(unclass(pca_coup$loadings), stringsAsFactors = F)
+factors <- data.frame(
+  "ID" = rownames(df),
+  "factor" = apply(df[1:ncol(df)], 1, function(x) colnames(df)[which.max(abs(x))]),
+  stringsAsFactors = F)
+
+
+
+# Creating network using igraph
+coup <- read.csv("tables/coup_matrix.csv", row.names = 1) # if you need to load network
+g <- graph_from_adjacency_matrix(as.matrix(coup), mode = "undirected",
+                                 weighted = T, diag = F)
+summary(g) #check graph
+V(g)$name # check vertex
+
+# Vertex Attributes
+V(g)$ID <- rownames(factors)
+V(g)$factor <- factors$factor
+V(g)$degree <- degree(g, normalized = F)
+V(g)$Ndegree <- degree(g, normalized = T)
+V(g)$betweeness <- betweenness(g, directed = F)
+V(g)$Nbetweeness <- betweenness(g, directed = F, normalized = T)
+V(g)$transitivity <- transitivity(g) # clustering coefficient
+V(g)$closeness <- closeness(g)
+names(get.vertex.attribute(g))
+
+write.csv2(as.data.frame(vertex_attr(g)), "tables/coup_vertex_attributes.csv")
+
+# Subnetworks
+sub_df <- data.frame()
+ties_between_group <- mixingmatrix(asNetwork(g), "factor")
+for (i in unique(na.omit(V(g)$factor))){
+  new_g <- induced_subgraph(g, which(V(g)$factor==as.character(i)))
+  total_size <- length(V(g))
+  size <- length(V(new_g))
+  centralization_degree <- centr_degree(new_g)$centralization
+  sub_density <- edge_density(new_g, loops = F)
+  sub_cohesion_igraph <- cohesion(new_g)
+  total_ties_between_factors <-  sum(ties_between_group$matrix[which(rownames(ties_between_group$matrix) %in% as.character(i)),
+                                                               -which(colnames(ties_between_group$matrix) %in% as.character(i))])
+  possible_ties_between_factors <- size * (total_size - size)
+  cohesion_manual <- sub_density * total_ties_between_factors / possible_ties_between_factors
+  dat <- data.frame("factor" = as.factor(i),
+                    "size"= size,
+                    "centralization_degree" = centralization_degree,
+                    "density" = sub_density,
+                    "cohesion" = sub_cohesion_igraph,
+                    "total_size_network" = total_size,
+                    "total_ties_between_factors" = total_ties_between_factors,
+                    "possible_ties_between_factors" = possible_ties_between_factors,
+                    "cohesion_manual" = cohesion_manual)
+  sub_df <- rbind(sub_df, dat)
+}
+write.csv2(sub_df, "tables/coup_subnetwork_stats.csv")
+
+# Exporting to DOT (This is the best to Gephi)
+write_graph(g, "data/coup_network.dot", format = "dot")
