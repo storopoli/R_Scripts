83eec42b98c1769afb5a89f6f84c41cb4ae92cbf
José Eduardo Storopoli <thestoropoli@gmail.com>
2020-08-02 04:53:11 -0300

    Create 8-schools.R

---
+++ Stan/8-schools.R
@@ -0,0 +1,43 @@
+library(rstan)
+library(microbenchmark)
+
+schools_dat <- list(J = 8, 
+                    y = c(28,  8, -3,  7, -1,  1, 18, 12),
+                    sigma = c(15, 10, 16, 11,  9, 11, 10, 18))
+
+model <- "
+data {
+  int<lower=0> J;
+  real y[J];
+  real<lower=0> sigma[J];
+}
+
+parameters {
+  real mu;
+  real<lower=0> tau;
+  real theta[J];
+}
+
+model {
+  mu ~ normal(0, 5);
+  tau ~ cauchy(0, 5);
+  theta ~ normal(mu, tau);
+  y ~ normal(theta, sigma);
+}
+
+generated quantities {
+    vector[J] log_likelihood;
+    vector[J] y_hat;
+    for (j in 1:J) {
+        log_likelihood[j] = normal_lpdf(y[j] | theta[j], sigma[j]);
+        y_hat[j] = normal_rng(theta[j], sigma[j]);
+    }
+}
+"
+
+result <- microbenchmark(
+  fit <- stan(model_code = model, data = schools_dat, chains = 4, iter = 2000),
+  times = 1
+)
+
+print(result)
