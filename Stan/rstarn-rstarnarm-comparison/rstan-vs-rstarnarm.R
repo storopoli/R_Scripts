3f3b8f3a44c2511e02d8cd32ce02b1a4e6b3d416
José Eduardo Storopoli <thestoropoli@gmail.com>
2020-08-12 05:22:01 -0300

    Renamed Stan Stuff

--- Stan/rstarn-rstarnarm-comparison/rstan-vs-rstarnarm.R
+++ Stan/rstarn-rstarnarm-comparison/rstan-vs-rstarnarm.R
@@ -5,34 +5,11 @@ library(microbenchmark)
 # All simulations are Stan's standard
 # 4 parallel chains with 2,000 iter
 
-data(mtcars)
+# Compile Models
 
-# Using rstnarm
-fit_arm <- stan_glm(mpg ~ hp + wt, data=mtcars)
-
-# using stan
-data <-  list(
-  N = nrow(mtcars),
-  K = 2,
-  X = matrix(data = c(mtcars$hp, mtcars$wt), ncol = 2),
-  y = mtcars$mpg)
-
-model <- stan_model("mtcars.stan")
-model_std <- stan_model("mtcars_std.stan")
-model_qr <- stan_model("mtcars_qr.stan")
-
-fit <- sampling(model, data = data)
-
-fit_std <- sampling(model_std, data = data)
-
-fit_qr <- sampling(model_qr, data = data)
-
-benchmark <- microbenchmark(
-  sampling(model, data = data),      # 12s
-  sampling(model_std, data = data),  # 12s
-  sampling(model_qr, data = data),   # 11s
-  times = 1
-)
+model <- stan_model("san.stan")
+model_std <- stan_model("stan_std.stan")
+model_qr <- stan_model("stan_qr.stan")
 
 # Big Dataset
 N <- 10000  # 10,000 obs
