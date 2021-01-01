10d0fa662783f7194d6650011e1d5dca3d4c2eee
Jose Storopoli <thestoropoli@gmail.com>
2021-01-01 10:01:25 -0300

    Create hexSticker_UNINOVE.R

---
+++ hexSticker_UNINOVE.R
@@ -0,0 +1,8 @@
+library(hexSticker)
+
+imgurl <- "https://github.com/storopoli/Estatistica-Bayesiana/raw/master/images/uninove.png"
+sticker(imgurl,
+        package="CIS",
+        p_size=12,
+        s_x=1, s_y=.75, s_width=.8,
+        filename="~/Desktop/UNINOVE.png")
