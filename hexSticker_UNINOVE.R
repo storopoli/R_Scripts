8eed6267773c5602886bfd06206fffc012ff29fc
José Eduardo Storopoli <thestoropoli@gmail.com>
2021-01-19 16:26:31 -0300

    Update hexSticker_UNINOVE.R

--- hexSticker_UNINOVE.R
+++ hexSticker_UNINOVE.R
@@ -5,4 +5,10 @@ sticker(imgurl,
         package="CIS",
         p_size=12,
         s_x=1, s_y=.75, s_width=.8,
-        filename="~/Desktop/UNINOVE.png")
+        filename="~/Desktop/UNINOVE_CIS.png")
+
+sticker(imgurl,
+        package="PPGA",
+        p_size=12,
+        s_x=1, s_y=.75, s_width=.8,
+        filename="~/Desktop/UNINOVE_PPGA.png")
