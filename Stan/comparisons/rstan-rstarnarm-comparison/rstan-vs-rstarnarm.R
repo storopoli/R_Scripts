0cd2e13a3b74bcdee4901c3a619a9ca469271f46
Jose Eduardo Storopoli <thestoropoli@gmail.com>
2021-01-29 15:27:52 -0300

    changed rstan stuff

--- Stan/comparisons/rstan-rstarnarm-comparison/rstan-vs-rstarnarm.R
+++ Stan/comparisons/rstan-rstarnarm-comparison/rstan-vs-rstarnarm.R
@@ -1,15 +1,12 @@
-library(rstanarm)
 library(rstan)
+library(cmdstanr)
+library(brms)
+library(rstanarm)
 library(microbenchmark)
+library(magrittr)
 
 # All simulations are Stan's standard
 # 4 parallel chains with 2,000 iter
-
-# Compile Models
-
-model <- stan_model("stan.stan")
-model_std <- stan_model("stan_std.stan")
-model_qr <- stan_model("stan_qr.stan")
 
 # Big Dataset
 N <- 10000  # 10,000 obs
@@ -27,10 +24,30 @@ data_sim <- list(
   y = y
 )
 
+# Compile Models
+
+model <- stan_model("stan.stan")
+model_std <- stan_model("stan_std.stan")
+model_qr <- stan_model("stan_qr.stan")
+
+cmdmodel <- cmdstan_model("stan.stan")
+cmdmodel_std <- cmdstan_model("stan_std.stan")
+cmdmodel_qr <- cmdstan_model("stan_qr.stan")
+
+brmmodel <- make_stancode(bf(y ~ .), data = as.data.frame(cbind(X,y))) %>% stan_model(model_code = .)
+brmmodel_qr <- make_stancode(bf(y ~ ., decomp = "QR"), data = as.data.frame(cbind(X,y))) %>% stan_model(model_code = .)
+
 benchmark <- microbenchmark(
-  sampling(model, data = data_sim),                   # 993s
-  sampling(model_std, data = data_sim),               # 970s
-  sampling(model_qr, data = data_sim),                # 856s
-  stan_glm(y ~ ., data = as.data.frame(cbind(X,y))),  # 13s
+  sampling(model, data = data_sim),                              # 993s
+  sampling(model_std, data = data_sim),                          # 970s
+  sampling(model_qr, data = data_sim),                           # 856s
+  cmdmodel$sample(data = data_sim),                              #
+  cmdmodel_std$sample(data = data_sim),                          #
+  cmdmodel_qr$sample(data = data_sim),                           #
+  sampling(brmmodel, data = data_sim),                           #
+  sampling(brmmodel_qr, data = data_sim),                        # 993s
+  stan_glm(y ~ ., data = as.data.frame(cbind(X,y))),             # 13s
+  stan_glm(y ~ ., data = as.data.frame(cbind(X,y)), QR = TRUE),  # 13s
   times = 1
 )
+print(benchmark, unit = "s")
