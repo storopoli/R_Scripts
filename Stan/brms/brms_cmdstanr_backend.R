c94515eb32af98eecdd6af6ea461fece5b55ae21
Jose Eduardo Storopoli <thestoropoli@gmail.com>
2021-02-11 17:07:40 -0300

    brms benchmarks using normalize = FALSE

--- Stan/brms/brms_cmdstanr_backend.R
+++ Stan/brms/brms_cmdstanr_backend.R
@@ -3,7 +3,17 @@ library(dplyr)
 library(cmdstanr)
 mtcars <- mtcars %>% mutate(gear = gear %>% factor)
 
-#36s
-system.time(brm(data=mtcars, formula=bf(mpg~gear, sigma ~ gear), cores = 4))
-# 13s
-system.time(brm(data=mtcars, formula=bf(mpg~gear, sigma ~ gear), cores = 4, backend="cmdstanr"))
+# 30.4s (0.4s)
+system.time(brm(
+    data = mtcars, formula = bf(mpg ~ gear, sigma ~ gear), cores = 4, backend = "rstan"
+))
+
+# 8.71s (0.2s)
+system.time(brm(
+    data = mtcars, formula = bf(mpg ~ gear, sigma ~ gear), cores = 4, backend = "cmdstanr"
+))
+
+# 7.9s (0.2s)
+system.time(brm(
+    data = mtcars, formula = bf(mpg ~ gear, sigma ~ gear), cores = 4, backend = "cmdstanr", normalize = FALSE
+))
