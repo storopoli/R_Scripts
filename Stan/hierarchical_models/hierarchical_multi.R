1dc7eb145fbb5daf1e3b669086f450464f82a6d3
Jose Storopoli <thestoropoli@gmail.com>
2021-02-27 05:24:23 -0300

    more hierarchical correlated stuff from Stan

--- Stan/hierarchical_models/hierarchical_multi.R
+++ Stan/hierarchical_models/hierarchical_multi.R
@@ -11,6 +11,7 @@ u <- mpg %>%
     summarise_all(mean) %>%
     select(-class) %>%
     bind_cols(1, .) %>%
+    mutate_all(function(x) 1 / x) %>%
     t() %>%
     as.matrix() %>%
     unname()
@@ -27,30 +28,23 @@ dat <- list(
 )
 
 # Vanilla Multivariate Hierarchical Model
-# 8.5 M1 (89 divergences)
+# Total 20.6s (1 divergences)
 vanilla <- cmdstan_model(here::here("Stan", "hierarchical_models", "hierarchical_multi.stan"))
 vanilla_fit <- vanilla$sample(data = dat)
-vanilla_fit$summary()
+print(vanilla_fit$summary(), n = 90)
 
 # Standardized Predictors (QR) Hierarchical model
-# 2.5s Total / 1.3s M1
-standard <- cmdstan_model(here::here("Stan", "hierarchical_models", "standard.stan"))
+# 32.7s Total (3 divergences))
+standard <- cmdstan_model(here::here("Stan", "hierarchical_models", "standard_multi.stan"))
 standard_fit <- standard$sample(data = dat)
-print(standard_fit$summary(), n = 15)
-
-# 2.2s M1
-# Non-Centered Parameterization Standardized Predictors (QR) Hierarchical model
-standard_ncp <- cmdstan_model(here::here("Stan", "hierarchical_models", "ncp_standard.stan"))
-standard_ncp_fit <- standard_ncp$sample(data = dat)
-print(standard_ncp_fit$summary(), n = 22)
+print(standard_fit$summary(), n = 100)
 
 # brms
-# 7.7s M1 (49 divergences)
+# Total 10.9s M1 (32 divergences)
 brms_fit <- brm(
     bf(hwy ~ 1 + (1 + displ + year | class), decomp = "QR"),
     data = mpg,
     prior = c(
-        set_prior("normal(0, 20)", class = "intercept"),
         set_prior("lkj(2)", class = "cor"),
         set_prior("cauchy(0,2)", class = "sd")
     )
--- Stan/hierarchical_models/standard_multi.stan
+++ Stan/hierarchical_models/standard_multi.stan
@@ -8,6 +8,16 @@ data {
   matrix[L, J] u;               // group predictors transposed
   vector[N] y;                  // the response variable
 }
+transformed data {
+  matrix[N, K] Q_ast;
+  matrix[K, K] R_ast;
+  matrix[K, K] R_ast_inverse;
+
+  // thin and scale the QR decomposition
+  Q_ast = qr_thin_Q(X) * sqrt(N - 1);
+  R_ast = qr_thin_R(X) / sqrt(N - 1);
+  R_ast_inverse = inverse(R_ast);
+}
 parameters {
   real alpha;                                // Population-level Intercept
   matrix[K, J] z;                            // Non-centered Correlation Matrix
@@ -20,7 +30,7 @@ transformed parameters {
   // computing the group-level error SD, based on non-centered parameterization
   vector<lower=0>[K] tau = 2.5 * tan(tau_unif);
   // computing the group-level coefficient, based on non-centered parameterization
-  matrix[K, J] beta = gamma * u + diag_pre_multiply(tau, L_Omega) * z;
+  matrix[K, J] theta = gamma * u + diag_pre_multiply(tau, L_Omega) * z;
 }
 model {
   // priors
@@ -32,8 +42,11 @@ model {
   // likelihood
   vector[N] mu;
   for(n in 1:N) {
-    mu[n] = X[n, ] * beta[, jj[n]];
+    mu[n] = X[n, ] * theta[, jj[n]];
   }
   y ~ normal(mu, sigma);
 }
-
+generated quantities {
+  matrix[K, J] beta; //reconstructed population-level regression coefficients
+  beta = R_ast_inverse * theta; // coefficients on X
+}
