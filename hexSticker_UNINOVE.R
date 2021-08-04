846de23d4596ce622f27f89cd81978d2ddcce1b7
Jose Storopoli <thestoropoli@gmail.com>
2021-08-04 17:59:57 -0300

    new hexstickers

--- hexSticker_UNINOVE.R
+++ hexSticker_UNINOVE.R
@@ -1,14 +1,26 @@
 library(hexSticker)
 
-imgurl <- "https://github.com/storopoli/Estatistica-Bayesiana/raw/master/images/uninove.png"
-sticker(imgurl,
-        package="CIS",
-        p_size=12,
-        s_x=1, s_y=.75, s_width=.8,
-        filename="~/Desktop/UNINOVE_CIS.png")
+# imgurl <- "https://github.com/storopoli/Estatistica-Bayesiana/raw/master/images/uninove.png"
+# imgurl <- "https://raw.githubusercontent.com/storopoli/Estatistica-Bayesiana/master/slides/logos/uninove_white.png"
+imgurl <- "https://raw.githubusercontent.com/storopoli/Estatistica-Bayesiana/master/slides/logos/uninove_black.png"
 
 sticker(imgurl,
-        package="PPGA",
-        p_size=12,
-        s_x=1, s_y=.75, s_width=.8,
-        filename="~/Desktop/UNINOVE_PPGA.png")
+        package = "CIS",
+        p_size = 22,
+        s_x = 1, s_y = .75, s_width = .8,
+        filename = here::here("images", "UNINOVE_CIS.png")
+)
+
+sticker(imgurl,
+        package = "PPGA",
+        p_size = 22,
+        s_x = 1, s_y = .75, s_width = .8,
+        filename = here::here("images", "UNINOVE_PPGA.png")
+)
+
+sticker(imgurl,
+        package = "PPGI",
+        p_size = 22,
+        s_x = 1, s_y = .75, s_width = .8,
+        filename = here::here("images", "UNINOVE_PPGI.png")
+)
